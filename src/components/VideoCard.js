96973e6833f07798ada861bc794e829f31a9f808
Rokas <rookas.rudzenskas@gmail.com>
2021-05-12 11:00:54 +0300

    Rokas

--- src/components/VideoCard.js
+++ src/components/VideoCard.js
@@ -3,6 +3,7 @@ import "./styles/VideoCard.css";
 import TextField from "@material-ui/core/TextField";
 import Modal from "@material-ui/core/Modal";
 import { makeStyles } from '@material-ui/core/styles';
+import Button from "@material-ui/core/Button";
 
 
 function rand() {
@@ -36,14 +37,20 @@ const useStyles = makeStyles((theme) => ({
 const VideoCard = ({ thumbnailUrl, isCustom, channelImageUrl, title, channelName, numOfViews, postedDate }) => {
 
     const [open, setOpen] = useState(false);
+    const [input, setInput] = useState('');
+    const [imageUrl, setImageUrl] = useState('');
+    // console.log(input);
     const classes = useStyles();
     // getModalStyle is not a pure function, we roll the style only on the first render
     const [modalStyle] = React.useState(getModalStyle);
 
-    const handleClose = () => {
+    const handleClose = (e) => {
         // save url logic here
+        e.preventDefault();
+        setImageUrl(input);
         setOpen(false);
-    }
+        console.log(imageUrl);
+    };
 
 
     return (
@@ -53,15 +60,21 @@ const VideoCard = ({ thumbnailUrl, isCustom, channelImageUrl, title, channelName
             <Modal
                 open={open} onClose={handleClose}>
                 <div style={modalStyle} className={classes.paper}>
-                <TextField />
+                    <form action="">
+                        {/* what we type is going to be saved in the input state. but then we click*/}
+                        {/* submit, we have that url to the image  state*/}
+                        <TextField value={input} onChange={e => setInput(e.target.value)}/>
+                        <Button type="submit" onClick={handleClose}>Set Image URL</Button>
+                    </form>
+
                 </div>
             </Modal>
 
             {/* Defining if car is custom or not*/}
-            {isCustom ? (
-                <button onClick={e => setOpen(true)}>Upload thumbnail</button>
+            {isCustom && !imageUrl ? (
+                <button onClick={e => setOpen(true)}>Select thumbnail</button>
             ) : (
-                <img  className="videoCard__thumbnail" src=""/>
+                <img  className="videoCard__thumbnail" src={imageUrl}/>
             )}
 
 
