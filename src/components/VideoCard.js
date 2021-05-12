427fec69092a958d238981dff85105430cd54068
Rokas <rookas.rudzenskas@gmail.com>
2021-05-12 08:40:29 +0300

    Rokas

--- src/components/VideoCard.js
+++ src/components/VideoCard.js
@@ -12,10 +12,10 @@ const VideoCard = ({ thumbnailUrl, channelImageUrl, title, channelName, numOfVie
                 </div>
 
                 <div className="videoCard__infoText">
-                    <p>{title}</p>
-                    <p>{channelName}</p>
-                    <p>{numOfViews}</p>
-                    <p>{postedDate}</p>
+                    <p className="videoCard__infoTitle">{title}</p>
+                    <p className="videoCard__channelName">{channelName}</p>
+                    <p className="videoCard__numOfViews">{numOfViews}</p>
+                    <p className="videoCard__postedDate">{postedDate}</p>
                 </div>
             </div>
         </div>
