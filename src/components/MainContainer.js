921a05c3f6695fc98162dbd427ab31a1f3584ed9
Rokas <rookas.rudzenskas@gmail.com>
2021-05-12 08:04:56 +0300

    Rokas

--- src/components/MainContainer.js
+++ src/components/MainContainer.js
@@ -4,7 +4,7 @@ import "./styles/MainContainer.css";
 const MainContainer = () => {
     return (
         <div className="mainContainer">
-            <h1>This is main container</h1>
+            <VideoCard />
         </div>
     );
 };
---
+++ src/components/VideoCard.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import "./styles/VideoCard.css";
+
+const VideoCard = ({ thumbnailUrl, title, channelName, numOfViews, postedDate }) => {
+    return (
+        <div className="videoCard">
+
+        </div>
+    );
+};
+
+export default VideoCard;
