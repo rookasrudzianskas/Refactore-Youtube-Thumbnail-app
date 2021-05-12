3f4f369aa2d8a735185a73a2f7de5352f2d8682e
Rokas <rookas.rudzenskas@gmail.com>
2021-05-12 08:32:59 +0300

    Rokas

--- src/components/MainContainer.js
+++ src/components/MainContainer.js
@@ -1,9 +1,20 @@
 import React from 'react';
 import "./styles/MainContainer.css";
+import VideoCard from "./VideoCard";
 
 const MainContainer = () => {
     return (
         <div className="mainContainer">
+            <VideoCard
+                        channelImageUrl="https://pbs.twimg.com/profile_images/1350895249678348292/RS1Aa0iK_400x400.jpg"
+                       postedDate="3 days ago"
+                       numOfViews="6.4M Views"
+                       channelName="Rookas Rudzianskas"
+                       title="Something cool is coming up?! 🚀"
+                       thumbnailUrl="https://d34tp322e0pcja.cloudfront.net/wp-content/uploads/2019/08/20210015/colors-stand-out.jpg"/>
+            <VideoCard />
+            <VideoCard />
+            <VideoCard />
             <VideoCard />
         </div>
     );
--- src/components/VideoCard.js
+++ src/components/VideoCard.js
@@ -1,10 +1,23 @@
 import React from 'react';
 import "./styles/VideoCard.css";
 
-const VideoCard = ({ thumbnailUrl, title, channelName, numOfViews, postedDate }) => {
+const VideoCard = ({ thumbnailUrl, channelImageUrl, title, channelName, numOfViews, postedDate }) => {
     return (
         <div className="videoCard">
+            <img className="videoCard__thumbnail" src={thumbnailUrl} alt=""/>
 
+            <div className="videoCard__info">
+                <div className="videoCard__channelImageContainer">
+                    <img src={channelImageUrl} alt=""/>
+                </div>
+
+                <div className="videoCard__infoText">
+                    <p>{title}</p>
+                    <p>{channelName}</p>
+                    <p>{numOfViews}</p>
+                    <p>{postedDate}</p>
+                </div>
+            </div>
         </div>
     );
 };
