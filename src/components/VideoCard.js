57fdc0407ed30a8e3092f53c626814b98a0b0206
Rokas <rookas.rudzenskas@gmail.com>
2021-05-12 10:35:05 +0300

    Rokas

--- src/components/VideoCard.js
+++ src/components/VideoCard.js
@@ -2,11 +2,43 @@ import React, {useState} from 'react';
 import "./styles/VideoCard.css";
 import TextField from "@material-ui/core/TextField";
 import Modal from "@material-ui/core/Modal";
+import { makeStyles } from '@material-ui/core/styles';
+
+
+function rand() {
+    return Math.round(Math.random() * 20) - 10;
+}
+
+function getModalStyle() {
+    const top = 50;
+    const left = 50;
+
+    return {
+        top: `${top}%`,
+        left: `${left}%`,
+        transform: `translate(-${top}%, -${left}%)`,
+    };
+}
+
+const useStyles = makeStyles((theme) => ({
+    paper: {
+        position: 'absolute',
+        width: 400,
+        backgroundColor: theme.palette.background.paper,
+        border: '2px solid #000',
+        boxShadow: theme.shadows[5],
+        padding: theme.spacing(2, 4, 3),
+    },
+}));
+
 
 
 const VideoCard = ({ thumbnailUrl, isCustom, channelImageUrl, title, channelName, numOfViews, postedDate }) => {
 
     const [open, setOpen] = useState(false);
+    const classes = useStyles();
+    // getModalStyle is not a pure function, we roll the style only on the first render
+    const [modalStyle] = React.useState(getModalStyle);
 
     const handleClose = () => {
         // save url logic here
@@ -16,10 +48,14 @@ const VideoCard = ({ thumbnailUrl, isCustom, channelImageUrl, title, channelName
 
     return (
         <div className="videoCard">
+
             <Modal
                 open={open} onClose={handleClose}>
+                <div style={modalStyle} className={classes.paper}>
                 <TextField />
+                </div>
             </Modal>
+
             {isCustom ? (
                 <button onClick={e => setOpen(true)}>Upload thumbnail</button>
             ) : (
