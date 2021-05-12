607d2b4f2a3747f68d17330190a950ef33bb2bc1
Rokas <rookas.rudzenskas@gmail.com>
2021-05-12 07:34:21 +0300

    Rokas

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,12 @@ import "./styles/Header.css";
 const Header = () => {
     return (
         <div className="header">
-
+            <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b8/YouTube_Logo_2017.svg/1024px-YouTube_Logo_2017.svg.png" alt=""/>
+        {/*    Youtube logo*/}
+           <h1>Search Box</h1>
+        {/*    Search box   */}
+            <h1>Right side</h1>
+        {/*    Right side logo  */}
         </div>
     );
 };
